80c0ef8f46e9c6e40b6c42f48d7677737666b7ea
Sameel <sameelnaqvi@gmail.com>
2023-11-27 20:59:24 -0500

    Switch screen functionality, launchscreen/appIcon

--- Summarize/CollectionsViewController.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class CollectionsViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .lightGray
-    }
-}
-
